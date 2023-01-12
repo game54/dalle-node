4b14073e1a83df14f52e1baac98dc963e3d16c40
game54 <ap_nbs@hotmail.com>
2023-01-13 00:33:11 +0200

    updated promises

--- dalle-node.js
+++ dalle-node.js
@@ -1,4 +1,4 @@
-import got from 'got';
+import got from "got";
 
 export class DalleError extends Error {
   constructor(response) {
@@ -14,55 +14,73 @@ export class Dalle {
   }
 
   async generate(prompt) {
-    let task = await got.post(`${this.url}/tasks`, {
-      json: {
-        task_type: "text2im",
-        prompt: {
-          caption: prompt,
-          batch_size: 4,
-        },
-      },
-      headers: {
-        Authorization: `Bearer ${ this.bearerToken }`
-      }
-    }).json();
+    return new Promise(async (resolve, reject) => {
+      let task = await got
+        .post(`${this.url}/tasks`, {
+          json: {
+            task_type: "text2im",
+            prompt: {
+              caption: prompt,
+              batch_size: 1,
+            },
+          },
+          headers: {
+            Authorization: `Bearer ${this.bearerToken}`,
+          },
+        })
+        .json();
 
-    const refreshIntervalId = setInterval(async () => {
-      task = await this.getTask(task.id)
+      const refreshIntervalId = setInterval(async () => {
+        task = await this.getTask(task.id);
 
-      switch (task.status) {
-        case "succeeded":
-          clearInterval(refreshIntervalId);
-          return task.generations;
-        case "rejected":
-          clearInterval(refreshIntervalId);
-          return new DalleError(task.status_information);
-        case "pending":
-      }
-    }, 2000);
+        switch (task.status) {
+          case "succeeded":
+            clearInterval(refreshIntervalId);
+            resolve(task.generations);
+            break;
+          case "rejected":
+            clearInterval(refreshIntervalId);
+            reject(new DalleError(task.status_information));
+            break;
+          case "pending":
+            break;
+        }
+      }, 2000);
+    });
   }
 
   async getTask(taskId) {
-    return await got.get(`${this.url}/tasks/${ taskId }`, {
-      headers: {
-        Authorization: "Bearer " + this.bearerToken,
-      },
-    }).json();
+    return await got
+      .get(`${this.url}/tasks/${taskId}`, {
+        headers: {
+          Authorization: "Bearer " + this.bearerToken,
+        },
+      })
+      .json();
   }
-  
+
   async list(options = { limit: 50, fromTs: 0 }) {
-    return await got.get(`${this.url}/tasks?limit=${ options.limit }${ options.fromTs ? `&from_ts=${ options.fromTs }` : '' }`, {
-        headers: {
-          Authorization: "Bearer " + this.bearerToken,
-        },
-      }).json();
+    return await got
+      .get(
+        `${this.url}/tasks?limit=${options.limit}${
+          options.fromTs ? `&from_ts=${options.fromTs}` : ""
+        }`,
+        {
+          headers: {
+            Authorization: "Bearer " + this.bearerToken,
+          },
+        }
+      )
+      .json();
   }
-  
+
   async getCredits() {
-    return await got.get(`${ this.url }/billing/credit_summary`, {
+    return await got
+      .get(`${this.url}/billing/credit_summary`, {
         headers: {
           Authorization: "Bearer " + this.bearerToken,
         },
-      }).json();
+      })
+      .json();
   }
 }
